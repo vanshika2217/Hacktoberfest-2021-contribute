fa10b132c7660af6189b403503c55079d0cbd208
Sayam <sayamgul06@gmail.com>
2021-10-06 20:53:09 +0530

    Add test cases

--- Cpp/checkEvenOdd.cpp
+++ Cpp/checkEvenOdd.cpp
@@ -1,13 +1,20 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    if(n % 2 == 0)
-        cout << "Even";
-    else {
-        cout << "Odd";
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n % 2 == 0)
+            cout << "Even";
+        else
+        {
+            cout << "Odd";
+        }
     }
 }
