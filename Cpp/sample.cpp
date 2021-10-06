ed75f76b2c5d871c9f063c128a2eff01de21cfc9
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2021-10-06 17:04:24 +0530

    The Sum of the value
    Run this code and Find the sum of the value

---
+++ Cpp/sample.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a,b,c,sum;
+    cout<<"Entered the 1st Digit "<<endl;
+    cin>>a;
+    cout<<"Entered the 1st Digit "<<endl;
+    cin>>b;
+    cout<<"Entered the 1st Digit "<<endl;
+    cin>>c;
+
+    sum = a+b+c;
+    cout<<"The Sum of the value is "<<sum<<endl;
+    return 0;
+}
