182229ce653eacdcb527396a756cd46191647b65
pouya <pouyaanp0@gmail.com>
2021-10-06 21:47:33 +0330

    take the number of fib rows and number of fib index

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -6,3 +6,19 @@ def fib(n):
         a, b = b, b+a
         count += 1
     print()
+
+numberList = []
+
+number = int(input("Enter the number for fibo rows: "))
+if number >= 1 and number <= 100:
+    for i in range(number):
+        numbers = int(input("Enter the index of fibonacci: "))
+        if number >= 1 and number <= 100:
+            numberList.append(numbers)
+        else:
+            print("out of index")
+
+    for index, number in enumerate(numberList):
+        fib(numberList[index])
+else:
+    print("out of index")
