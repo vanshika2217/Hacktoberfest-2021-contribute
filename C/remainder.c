17e28dc841d521b1b700967ca31a74bd67a5843a
ShashankPatel0111 <shashank01patel11@gmail.com>
2021-10-06 23:40:30 +0530

    Added remainder in C and python

---
+++ C/remainder.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int t,a,b,i;
+    scanf("%d",&t);
+    for(i=0; i<n; i++)
+    {
+        scnaf("%d %d",&a,&b);
+        printf("%d",(a%b));
+    }
+    return 0;
+}
