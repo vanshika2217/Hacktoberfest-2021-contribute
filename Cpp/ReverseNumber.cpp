5502bb89cba3a42249f99ba0c94c80b667e396eb
Prachi Gupta <prachiannie17@gmail.com>
2021-10-07 10:54:58 +0530

    ReverseNumber

---
+++ Cpp/ReverseNumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//to reverse number
+	int t;
+	cin>>t;
+	int num,rev=0,d;
+
+	while(t--){
+		cin>>num;
+		rev=0;
+		while(num>0){
+			d=num%10;
+		    rev=rev*10+d;
+		    num=num/10;
+
+		}
+		cout<<rev<<endl;
+
+	}
+	return 0;
+}
